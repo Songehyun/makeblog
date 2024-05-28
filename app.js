f81a3256b25ff6cc47e784c09535c7a6a51bdcb5
송이현 <songehyun00@gmail.com>
2024-05-28 15:57:12 +0900

    html제목 변경

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const loadserver = require('./loadserver.js');
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET"){
-  loadserver("/", "index.html", req, res);
-  loadserver("/index.html", "index.html", req, res);
-  loadserver("/indexstyle.css", "indexstyle.css", req, res);
-  loadserver("/writenote.html", "writenote.html", req, res);
-  loadserver("/writenotestyle.css", "writenotestyle.css", req, res);
+  loadserver("/", "index.html", "text/html", req, res);
+  loadserver("/index.html", "index.html","text/html", req, res);
+  loadserver("/indexadd.js", "indexadd.js","text/javascript", req, res);
+  loadserver("/indexstyle.css", "indexstyle.css","text/css", req, res);
+  loadserver("/writenote.html", "writenote.html","text/html", req, res);
+  loadserver("/writenotestyle.css", "writenotestyle.css","text/css", req, res);
   } else if (req.method === "POST") {
-    if (req.url === "/test"){
+    if (req.url === "/submit"){
       let body = "";
       req.on("data", (chunk) => {
         body += chunk.toString();
@@ -21,19 +22,50 @@ const server = http.createServer((req, res) => {
         const writetitle = parsedData.get("writetitle");
         const writecontent = parsedData.get("writecontent");
 
-        const jsonData = {
-          writetitle : writetitle,
-          writecontent : writecontent
-        };
-        const jsonDataString = JSON.stringify(jsonData, null, 2);
-        fs.writeFile(path.join(__dirname, "data.json"), jsonDataString, (err) => {
+        let today = new Date();
+
+        const jsonData = `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>note</title>
+        </head>
+        <link rel="stylesheet" href="allnotestyle.css">
+        <body>
+          <div id="root">
+            <div>
+              <div>
+                <h2 onclick="location.href='./index.html'">${writetitle}</h2>
+              </div>
+            </div>
+            <div>
+              <div>
+                <pre>${writecontent}</pre>
+              </div>
+            </div>
+          </div>
+        </body>
+        </html>`;
+
+        // let addlink = today.toLocaleDateString()+writetitle
+        
+        fs.writeFile(path.join(__dirname, `${today.toLocaleDateString()}${writetitle}.html`), jsonData, (err) => {
           if (err) {
             res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8"});
             res.end("서버 자체 에러");
             return;
           }
-          res.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-          res.end();
+          fs.readFile('./index.html',(err,data)=>{
+            if(err){
+              res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8"});
+            res.end("서버 자체 에러");
+            return;
+            }
+            res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+            res.end(data);
+          });
         });
       });
     } else {
