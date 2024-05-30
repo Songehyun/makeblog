e6664e6183237b9a25a973895b4b9dfd69fd05cc
송이현 <songehyun00@gmail.com>
2024-05-30 18:08:42 +0900

    삭제까지 끝

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ let newdiv = '';
     let contype = flieUtils.getContentType(ext)
 
     if(req.url === url){
-      console.log(req.url);
       fs.readFile(filePath,(err,data)=>{
         if(err){
           res.writeHead(500,{"Content-Type":"text/plain; charset=UTF-8"});
@@ -68,9 +67,17 @@ let newdiv = '';
                   <pre>${writecontent}</pre>
                 </div>
               </div>
+              <div>
+                <form action="/delete" method="post">
+                <button type="submit" id="delete">삭제</button>
+                <button type="submit" id="modify">수정</button>
+                </form>
+              </div>
             </div>
           </body>
         </html>`;
+
+        newdiv += `<div onclick="location.href='./${writetitle}.html'">${writetitle}</div>`
 
         const indexData = `<!DOCTYPE html>
         <html lang="en">
@@ -100,8 +107,6 @@ let newdiv = '';
         </body>
         </html>`;
 
-        newdiv += `<div onclick="location.href='./${writetitle}.html'">${writetitle}</div>`
-
         // 파일 제목을 저장해서 불러오도록 도와주는 txt
         fs.readFile(path.join(__dirname, "./public/indexupdate.json"), (err, data) => {
           if(err){
@@ -123,36 +128,103 @@ let newdiv = '';
             console.log("오류")
           }
         });
+
         fs.writeFile(path.join(__dirname, `./public/${writetitle}.html`), htmlData, (err) => {
           if (err) {
             res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8"});
             res.end("서버 자체 에러");
             return;
           }
-          fs.readFile('./public/index.html',(err,data)=>{
-            if(err){
-              res.writeHead(500, {"Content-Type": "text/plain; charset=utf-8"});
-            res.end("서버 자체 에러");
-            return;
-            }
-            res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
-            res.end(data);
-          });
+          res.writeHead(302, { Location: "/" });
+          res.end();
         });
-
       });
+
+    } else if (req.url === "/delete") {
+      const deletepath = path.join(__dirname, "./public")
+      let allrefer = req.headers.referer
+      let sprefer = allrefer.split('/')[3];
+      let wordrefer = sprefer.split('.')[0];
+      let decoderefer = decodeURI(sprefer);
+      let decodeword = decodeURI(wordrefer);
+      fs.readdir(path.join(__dirname, "./public"), (err,data)=>{
+        for(let i = 0; i < data.length; i++){
+        if(data[i] === decoderefer){
+          fs.unlink(`${deletepath}/${decoderefer}`, (err) => {
+            if(err) {console.log("삭제못함");}
+          });
+        }
+      }
+      fs.readFile(path.join(__dirname, "./public/indexupdate.json"), (err, data) => {
+        if(err) {
+          console.log("에러떳음")
+        } else {
+          const parse = JSON.parse(data);
+          for(let y = 0; y < parse.length; y++){
+            if(parse[y] === decodeword){
+              parse.splice(y, 1);
+              newparse = JSON.stringify(parse);
+              fs.writeFile("./public/indexupdate.json",`${newparse}`,(err) => {
+                if(err){
+                  console.error(err);
+                } else {
+                  newdiv = '';
+                  fs.readFile(path.join(__dirname, "./public/indexupdate.json"), (err, data) => {
+                    const parse = JSON.parse(data);
+                    for(let i = 0; i < parse.length; i++){
+                    newdiv += `<div onclick="location.href='./${parse[i]}.html'">${parse[i]}</div>`;
+                    }
+                    const indexData = `<!DOCTYPE html>
+                    <html lang="en">
+                    <head>
+                      <meta charset="UTF-8">
+                      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>indexpage</title>
+                      <link rel="stylesheet" href="indexstyle.css">
+                    </head>
+                    <body>
+                      <div id="root">
+                        <div>
+                          <div id="HomeTitle">
+                            <h1 onclick="location.href='./index.html'">홈페이지 이름</h2>
+                          </div>
+                        </div>
+                        <div>
+                          <div id="MakeNote">
+                            <h2 onclick="location.href='./writenote.html'">글쓰기</h2>
+                          </div>
+                        </div>
+                        <div id="NoteTitle">
+                          ${newdiv}
+                        </div>
+                      </div>
+                    </body>
+                    </html>`;
+                    fs.writeFile(path.join(__dirname, "./public/index.html"), indexData, (err) => {
+                      if(err){
+                        console.log("오류")
+                      }
+                    });
+                  });
+                }
+              });
+            }
+          }
+        }
+      })
+      });
+      res.writeHead(302, { Location: "/" });
+      res.end();
     } else {
       res.writeHead(404, {"Content-Type": "text/plain; charset=utf-8"});
       res.end("404 code는 페이지를 찾을 수 없음");
     }
-  } 
-  else {
+  } else {
     res.writeHead(404, {"Content-Type": "text/plain; charset=utf-8"});
     res.end("404 code는 페이지를 찾을 수 없음");
   }
 });
-
-
 const PORT = 8080;
 server.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
