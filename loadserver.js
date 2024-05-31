21d2db8589f65359e18e1309a22e47ff61948e82
송이현 <songehyun00@gmail.com>
2024-05-31 17:24:37 +0900

    패치노트 만들기 완료

--- loadserver.js
+++ loadserver.js
@@ -1,45 +1,43 @@
 const flieUtils = {
-  getFliePath: function(url) {
+  getFliePath: function (url) {
     let fliePath;
-    if(url === "/") {
+    if (url === "/") {
       fliePath = "./public/index.html";
     } else {
       fliePath = "./public" + url;
     }
     return decodeURI(fliePath);
   },
-  getFlieExtension: function(fliePath){
-    const path = require('path');
+  getFlieExtension: function (fliePath) {
+    const path = require("path");
     let ext = path.extname(fliePath);
     return ext.toLowerCase();
   },
-  getContentType: function(ext){
+  getContentType: function (ext) {
     const mimeType = {
-      ".html" : "text/html; charset=utf-8",
-      ".css" : "text/css",
-      ".js" : "application/javascript",
-      ".json" : "application/json",
-      ".ico" : "image/x-icon"
+      ".html": "text/html; charset=utf-8",
+      ".css": "text/css",
+      ".js": "application/javascript",
+      ".json": "application/json",
+      ".ico": "image/x-icon",
     };
-    
-    if (mimeType.hasOwnProperty(ext)){
+
+    if (mimeType.hasOwnProperty(ext)) {
       return mimeType[ext];
     } else {
       return "text/plain";
     }
-  }
+  },
 };
 
-
 module.exports = flieUtils;
-
 
 // if (req.method === "GET"){
 //   let url = req.url
 //   let filePath = flieUtils.getFliePath(url)
 //   let ext = flieUtils.getFlieExtension(filePath)
 //   let contype = flieUtils.getContentType(ext)
-  
+
 //   if(req.url === url){
 //     console.log(req.url);
 //     fs.readFile(filePath,(err,data)=>{
